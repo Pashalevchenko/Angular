3748486bf6cabdac52f8c8d498590adea1d8de5f
Pasha <pashalevchenko1995@gmail.com>
2021-07-27 13:17:35 +0300

    new dz

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {IUser} from "../models/IUser";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private  url = 'https://jsonplaceholder.typicode.com/users'
+  constructor(private httpClient: HttpClient) {
+
+  }
+
+  getAllUsers(): Observable<IUser[]>{
+  return this.httpClient.get<IUser[]>(this.url)
+}
+
+
+  getAllUsersById(id: number): Observable<IUser[]>{
+  return this.httpClient.get<IUser[]>(this.url + '/' + id)
+}
+
+
+}
---
+++ src/app/user-details/user-details.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {IUser} from "../models/IUser";
+import {UserService} from "../services/user.service";
+
+@Component({
+  selector: 'app-user-details',
+  templateUrl: './user-details.component.html',
+  styleUrls: ['./user-details.component.scss']
+})
+export class UserDetailsComponent implements OnInit {
+
+  user: any;
+
+  constructor(private activatedRoute: ActivatedRoute, private userService: UserService) {
+    this.activatedRoute.params.subscribe(({id}) => {
+      this.userService.getAllUsersById(id).subscribe(value => this.user = value);
+
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+}
