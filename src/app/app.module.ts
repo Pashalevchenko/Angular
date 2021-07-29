aa84caf37fa133f0b5aa8d68dc2c49bec441b3af
Pasha <pashalevchenko1995@gmail.com>
2021-07-29 11:46:40 +0300

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {RouterModule, Routes} from "@angular/router";
+import {UsersComponent} from './components/users/users.component';
+import {HttpClientModule} from "@angular/common/http";
+import { UserComponent } from './components/user/user.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+import {UserDeactivatorService} from "./models/user-deactivator.service";
+
+let routes: Routes = [
+  {path: '', component: UsersComponent, canDeactivate: [UserDeactivatorService]},
+  {path: 'users/:id', component: UserDetailsComponent},
+  {path: 'users', redirectTo: '', pathMatch: 'full'},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserComponent,
+    UserDetailsComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
