02315e58bec7f37ab3db718987e5f31c33c55613
Pasha <pashalevchenko1995@gmail.com>
2021-07-30 13:37:26 +0300

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,25 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {RouterModule, Routes} from "@angular/router";
-import {UsersComponent} from './components/users/users.component';
 import {HttpClientModule} from "@angular/common/http";
-import { UserComponent } from './components/user/user.component';
-import { UserDetailsComponent } from './components/user-details/user-details.component';
-import {UserDeactivatorService} from "./models/user-deactivator.service";
+import { PostsComponent } from './components/posts/posts.component';
+import { PostComponent } from './components/post/post.component';
+import { PostDetailsComponent } from './components/post-details/post-details.component';
+
 
 let routes: Routes = [
-  {path: '', component: UsersComponent, canDeactivate: [UserDeactivatorService]},
-  {path: 'users/:id', component: UserDetailsComponent},
-  {path: 'users', redirectTo: '', pathMatch: 'full'},
+  {path: '', component: PostsComponent},
+  {path: 'posts/:id', component: PostDetailsComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent,
-    UserComponent,
-    UserDetailsComponent,
+    PostsComponent,
+    PostComponent,
+    PostDetailsComponent,
+
+
   ],
   imports: [
     BrowserModule,
---
+++ src/app/services/post.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {IPost} from "../models/IPost.";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+  private url = 'https://jsonplaceholder.typicode.com/posts'
+
+  constructor(private httpClient : HttpClient) { }
+
+  getPost(): Observable<IPost[]>{
+    return this.httpClient.get<IPost[]>(this.url)
+  }
+}
